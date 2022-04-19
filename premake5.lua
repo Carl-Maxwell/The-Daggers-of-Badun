cce4c1478fc69175118a8bf5ff75c6c6edd54a7e
= <carl.maxwell@gmail.com>
2022-04-19 18:13:50 +0100

    initial commit
    readme, premake file, pixel game engine, gitignore
    
    I think the premake file works, I copied it from the hazel-sauropod
    project I was working on

---
+++ premake5.lua
@@ -0,0 +1,184 @@
+workspace "the_daggers_of_badun"
+	architecture "x64"
+
+	configurations
+	{
+		"debug",
+		"release",
+		"dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+-- IncludeDir["GLFW"]  = "SauropodEngine/vendor/glfw/include"
+-- IncludeDir["Glad"]  = "SauropodEngine/vendor/Glad/include"
+-- IncludeDir["imgui"] = "SauropodEngine/vendor/imgui/"
+
+-- include "SauropodEngine/vendor/glfw"
+-- include "SauropodEngine/vendor/Glad"
+-- include "SauropodEngine/vendor/imgui"
+
+project "badun_engine"
+	location "badun_engine"
+	kind "SharedLib"
+	language "C++"
+
+	-- TODO: for some reason I wasn't able to use %{prj.location}
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}") 
+
+	-- pchheader "hzpch.h"
+	-- pchsource "%{prj.name}/src/hzpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+	links 
+	{ 
+
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		-- staticruntime "Off"
+		systemversion "latest"
+
+		-- defines 
+		-- {
+		
+		-- }
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/badun_sandbox"),
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/badun_game")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		buildoptions "/MDd" -- runtime library: multi-threaded debug DLL
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
+	
+project "badun_sandbox"
+	location "badun_sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}") 
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"badun_engine/vendor/spdlog/include",
+		"badun_engine/src"
+	}
+
+	links
+	{
+		"badun_engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		-- staticruntime "On"
+		systemversion "latest"
+
+	-- defines 
+	-- {
+	-- 	"HZ_PLATFORM_WINDOWS",
+	-- 	"_WINDLL"
+	-- }
+
+	filter "configurations:Debug"
+		-- defines "HZ_DEBUG"
+		buildoptions "/MDd" -- runtime library: multi-threaded debug DLL
+		symbols "On"
+
+	filter "configurations:Release"
+		-- defines "HZ_RELEASE"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
+
+	filter "configurations:Dist"
+		-- defines "HZ_DIST"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
+
+
+project "badun_game"
+	location "badun_game"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}") 
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"badun_engine/vendor/spdlog/include",
+		"badun_engine/src"
+	}
+
+	links
+	{
+		"badun_engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		-- staticruntime "On"
+		systemversion "latest"
+
+	-- defines 
+	-- {
+	-- 	"HZ_PLATFORM_WINDOWS",
+	-- 	"_WINDLL"
+	-- }
+
+	filter "configurations:Debug"
+		-- defines "HZ_DEBUG"
+		buildoptions "/MDd" -- runtime library: multi-threaded debug DLL
+		symbols "On"
+
+	filter "configurations:Release"
+		-- defines "HZ_RELEASE"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
+
+	filter "configurations:Dist"
+		-- defines "HZ_DIST"
+		buildoptions "/MD" -- runtime library: multi-threaded DLL
+		optimize "On"
---
+++ readme.md
@@ -0,0 +1,17 @@
+# The Daggers of Badun
+
+This is based off of the "Code-It-Yourself! 3D Graphics Engine" series from javidx9 on youtube
+
+- https://youtu.be/ih20l3pJoeU
+- https://youtu.be/XgMWc6LumG4
+- https://youtu.be/HXSuNxpCzdM
+- https://youtu.be/nBzCS-Y0FcY
+
+Hope to also implement collision detection, maybe based off of:
+
+- https://youtu.be/7Ik2vowGcU0
+
+Uses the pixel game engine (https://github.com/OneLoneCoder/olcPixelGameEngine) and spdlog
+
+
+(Project name was randomly generated on https://donjon.bin.sh/fantasy/name/#type=set)
