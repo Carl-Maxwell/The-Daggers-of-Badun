98e89fe9ef387f102965b06781481804012650f7
= <carl.maxwell@gmail.com>
2022-04-20 18:01:01 +0100

    add function for loading in obj files, & spaceship
    3d spaceship model that is

--- badun_engine/src/entry_point.cpp
+++ badun_engine/src/entry_point.cpp
@@ -1,5 +1,8 @@
 #include <vector>
 
+#include <fstream>
+#include <strstream>
+
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include "mtypedefs.h"
@@ -17,6 +20,44 @@ struct Triangle
 struct Mesh
 {
 	std::vector<Triangle> tris;
+
+	bool loadFromObjectFile(std::string s_filename)
+	{
+		std::ifstream f(s_filename);
+		if (!f.is_open())
+			return false;
+
+		// Local cache of vertices
+		std::vector<glm::vec3> file_vertices;
+
+		while (!f.eof()) 
+		{
+			char line[128];
+			f.getline(line, 128);
+
+			std::strstream s;
+			s << line;
+
+			char junk;
+
+			if (line[0] == 'v')
+			{
+				glm::vec3 v;
+				s >> junk >> v.x >> v.y >> v.z;
+				file_vertices.push_back(v);
+			} 
+			else if (line[0] == 'f') 
+			{
+				int f[3];
+				s >> junk >> f[0] >> f[1] >> f[2];
+				tris.push_back( {
+					file_vertices[ f[0] -1 ],
+					file_vertices[ f[1] -1 ],
+					file_vertices[ f[2] -1 ]
+				} );
+			}
+		}
+	}
 };
 
 class Application : public olc::PixelGameEngine
